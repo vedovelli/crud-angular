2269bb3f750cd2a668d134e5d2c89234bb4f6bf2
André Santos <afsdab@gmail.com>
2013-08-23 18:37:25 -0300

    Update ProdutosController.php

--- service/controller/ProdutosController.php
+++ service/controller/ProdutosController.php
@@ -2,9 +2,11 @@
 
 require_once 'model/ProdutosModel.php';
 
-class ProdutosController{
+class ProdutosController
+{
 
-	public function listarProdutos(){
+	public function listarProdutos()
+	{
 
 		$produtos = ProdutosModel::all();
 
@@ -22,29 +24,33 @@ class ProdutosController{
 
 	}
 
-	public function salvarProduto($produto){
+	public function salvarProduto($produto)
+	{
 		$produto = $this->object_to_array($produto);
 		$produto = ProdutosModel::create($produto);
 		return $produto->to_array();
 	}
 
-	public function atualizarProduto($produto){
+	public function atualizarProduto($produto)
+	{
 		//$produto = $this->object_to_array($produto);
 		$model = ProdutosModel::find($produto['id']);
 		$model->update_attributes($produto);
 		return $model->to_array();
 	}
 
-	public function removerProduto($id){
+	public function removerProduto($id)
+	{
 		$model = ProdutosModel::find($id);
 		return $model->delete();
 	}
 
-	private function object_to_array(stdClass $Class){
+	private function object_to_array(stdClass $Class)
+	{
 		#http://www.php.net/manual/en/language.types.object.php#102735		
 		$Class = (array)$Class;
-		foreach($Class as $key => $value){
-			if(is_object($value)&&get_class($value)==='stdClass'){
+		foreach($Class as $key => $value) {
+			if(is_object($value) && get_class($value)==='stdClass') {
 				$Class[$key] = self::object_to_array($value);
 			}
 		}
